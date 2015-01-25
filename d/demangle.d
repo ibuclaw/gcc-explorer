1dadcfa6a4b916f923dd137364cd6db0f6da7baf
Iain Buclaw <ibuclaw@gdcproject.org>
2015-01-25 18:48:36 +0000

    Fix demangle.d to prettify objdump output

--- d/demangle.d
+++ d/demangle.d
@@ -26,15 +26,53 @@ import std.stdio;
 import std.demangle;
 import std.regex;
 
-auto idRegex = ctRegex!("[_$a-zA-Z][_$a-zA-Z0-9]*", "g");
+auto idRegex = ctRegex!(`[_$a-zA-Z][_$a-zA-Z0-9]*`, `g`);
+auto sectRegex = ctRegex!(`[0-9]+\s+<(?P<symbol>.*)>:`);
+auto disRegex = ctRegex!(`\s+[0-9a-f]+:(?P<assembly>.*)`);
 
 void main() {
     string dem(Captures!(string) m) {
         return demangle(m.hit);
     }
 
+    bool skipSection = false;
+    bool skipEmpty = true;
+
     foreach (line; stdin.byLine()) {
         string s2 = cast(string)line;
+        // Ignoring this section.
+        if (skipSection) {
+            if (s2.length == 0)
+                skipSection = false;
+            continue;
+        }
+        // Ignore '/path/foo' and 'Disassembly of section...' lines
+        if (s2.length && s2[0] == '/' || s2[0] == 'D')
+            continue;
+        // Ignore initial set of blank lines outputted from objdump.
+        if (skipEmpty) {
+            if (s2.length == 0)
+                continue;
+            skipEmpty = false;
+        }
+
+        // Get the <symbol> from a section start.
+        auto m1 = matchFirst(s2, sectRegex);
+        if (m1)
+            s2 = m1["symbol"] ~ ":";
+        else {
+            // Remove prefixing hex locations from assembly.
+            auto m2 = matchFirst(s2, disRegex);
+            if (m2)
+                s2 = m2["assembly"];
+        }
+
+        // Skip over .text and other symbol disassemblies.
+        if (!skipSection && s2[0] == '.') {
+            skipSection = true;
+            continue;
+        }
+
         auto s = replaceAll!(dem)(s2, idRegex);
         writeln(s);
     }
