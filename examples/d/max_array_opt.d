ed781cc3e33b4a2c6c160dd0bfb3abce896c7e07
Iain Buclaw <ibuclaw@gdcproject.org>
2015-01-26 18:55:35 +0000

    Add a couple more examples, remove the _opt.d variants (DMD doesn't actually attempt any vectorization of these loops)

--- examples/d/max_array_opt.d
+++
@@ -1,7 +0,0 @@
-module max_array_opt;
-
-void maxArray(ref double[65536] x, ref double[65536] y)
-{
-    foreach (i; 0 .. 65536)
-        x[i] = ((y[i] > x[i]) ? y[i] : x[i]);
-}
--- examples/d/sum_over_array_opt.d
+++
@@ -1,9 +0,0 @@
-module sum_over_array_opt;
-
-int testFunction(int[] input)
-{
-    int sum = 0;
-    foreach(i; input)
-        sum += i;
-    return sum;
-}
